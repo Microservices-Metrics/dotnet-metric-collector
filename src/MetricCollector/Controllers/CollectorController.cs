53c70576ff54ae2e3576c51cf015c02ec2e74e51
Bruno Romano <brunoromano@gmail.com>
2025-05-15 23:58:04 -0300

    Creates collector controller

---
+++ src/MetricCollector/Controllers/CollectorController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricCollector.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CollectorController : ControllerBase
+{
+    private readonly ILogger<CollectorController> _logger;
+
+    public CollectorController(ILogger<CollectorController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Collect()
+    {
+        // if (metric == null)
+        // {
+        //     return BadRequest("Metric cannot be null");
+        // }
+
+        try
+        {
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error saving metric");
+
+            return StatusCode(500, "Internal server error");
+        }
+    }
+}
